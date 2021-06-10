3d7ffbdcba6b94a22301a7d60d33d067f93d01e2
pcktm <jakub@kopanko.com>
2021-06-10 22:06:00 +0200

    feat: add some pwa-related meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,13 @@ function Fizykapp({ Component, pageProps }) {
     <Head>
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name='apple-mobile-web-app-capable' content='yes' />
+      <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+      <meta name='apple-mobile-web-app-title' content="Fizykapp"/>
+      <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+      <meta name='application-name' content="Fizykapp" />
+      <meta name='format-detection' content='telephone=no' />
+      <meta name='mobile-web-app-capable' content='yes' />
       <meta
         name="viewport"
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
